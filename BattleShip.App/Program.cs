e65262be4b21ba039081236d8454123155d8085a
JulieLSR <julie30132@gmail.com>
2024-02-08 15:19:05 +0100

    Ajout des validations

--- BattleShip.App/Program.cs
+++ BattleShip.App/Program.cs
@@ -11,7 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => 
     new HttpClient
     {
-        BaseAddress = new Uri("https://localhost:5015")
+        BaseAddress = new Uri("http://localhost:5015")
     });
 
 await builder.Build().RunAsync();
